5a30dd23ccf7d75ae184c5d69a6c57555462a0ee
Archie Andrews <aa194093@falmouth.ac.uk>
2019-10-28 16:37:34 +0000

    started basic RTS camera

---
+++ Assets/Camera/Camera Inputs/RTSInput.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Basics.Camera
+{
+    [RequireComponent(typeof(RTSCameraMotor))]
+    public class RTSInput : MonoBehaviour
+    {
+        private RTSCameraMotor motor;
+
+        public KeyCode speedBoost = KeyCode.LeftShift;
+
+        private float hor, ver;
+        private float height = 0;
+
+        private Vector3 movementDir;
+
+        private void Awake()
+        {
+            motor = GetComponent<RTSCameraMotor>();
+        }
+
+        private void Update()
+        {
+            hor = Input.GetAxis("Horizontal");
+            ver = Input.GetAxis("Vertical");
+
+            movementDir.x = hor;
+            movementDir.z = ver;
+
+            float height = Input.GetAxis("Mouse ScrollWheel");
+
+            if(height != 0)
+                motor.AlterHeight(height);
+
+            if (Input.GetKey(speedBoost))
+                movementDir *= motor.GetSpeedBoostMultiplier();
+
+            motor.Move(movementDir);
+        }
+    }
+}
---
+++ Assets/Camera/RTSCameraMotor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Basics.Camera
+{
+    public class RTSCameraMotor : MonoBehaviour
+    {
+        [SerializeField]
+        private float movementSpeed = 5;
+
+        [SerializeField]
+        private float speedBoostMultiplier = 2;
+
+        [SerializeField]
+        private float heightFromFloor = 5;
+
+        public void Move(Vector3 direction)
+        {
+            transform.position += (direction * movementSpeed) * Time.deltaTime;
+            CheckHeight();
+        }
+
+        public float GetSpeedBoostMultiplier()
+        {
+            return speedBoostMultiplier;
+        }
+
+        public void AlterHeight(float heightIncrement)
+        {
+            heightFromFloor -= heightIncrement;
+            CheckHeight();
+        }
+
+        private void CheckHeight()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+            {
+                transform.position -= Vector3.up * (hit.distance - heightFromFloor);
+            }
+        }
+    }
+}
