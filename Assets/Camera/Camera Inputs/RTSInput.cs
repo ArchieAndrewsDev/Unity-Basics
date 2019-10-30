b7b1846ce7316e51e4345e258275c45c46c22b0b
Archie Andrews <aa194093@falmouth.ac.uk>
2019-10-30 16:59:33 +0000

    Added mouse movement

--- Assets/Camera/Camera Inputs/RTSInput.cs
+++ Assets/Camera/Camera Inputs/RTSInput.cs
@@ -12,7 +12,7 @@ namespace Basics.Camera
         private float hor, ver;
         private float height = 0;
 
-        private Vector3 movementDir;
+        private Vector3 movementDir, screenDir;
 
         private void Awake()
         {
@@ -20,6 +20,32 @@ namespace Basics.Camera
         }
 
         private void Update()
+        {
+            KeyBoardCtrl();
+            MouseCtrl();
+        }
+
+        private void MouseCtrl()
+        {
+            if (Input.mousePosition.x >= Screen.width - 1)
+                screenDir.x = 1;
+            else if (Input.mousePosition.x <= 1)
+                screenDir.x = -1;
+            else
+                screenDir.x = 0;
+
+            if (Input.mousePosition.y >= Screen.height - 1)
+                screenDir.z = 1;
+            else if (Input.mousePosition.y <= 1)
+                screenDir.z = -1;
+            else
+                screenDir.z = 0;
+
+                if(screenDir != Vector3.zero)
+            motor.Move(screenDir);
+        }
+
+        private void KeyBoardCtrl()
         {
             hor = Input.GetAxis("Horizontal");
             ver = Input.GetAxis("Vertical");
@@ -29,13 +55,14 @@ namespace Basics.Camera
 
             float height = Input.GetAxis("Mouse ScrollWheel");
 
-            if(height != 0)
+            if (height != 0)
                 motor.AlterHeight(height);
 
             if (Input.GetKey(speedBoost))
                 movementDir *= motor.GetSpeedBoostMultiplier();
 
-            motor.Move(movementDir);
+            if(movementDir != Vector3.zero)
+                motor.Move(movementDir);
         }
     }
 }
--- Assets/Camera/RTSCameraMotor.cs
+++ Assets/Camera/RTSCameraMotor.cs
@@ -6,12 +6,25 @@ namespace Basics.Camera
     {
         [SerializeField]
         private float movementSpeed = 5;
-
         [SerializeField]
         private float speedBoostMultiplier = 2;
+        [SerializeField]
+        private float heightSmoother = .1f;
 
         [SerializeField]
         private float heightFromFloor = 5;
+
+        Vector3 targetHeight, velocity;
+
+        private void Awake()
+        {
+            targetHeight = transform.position;
+        }
+
+        private void Update()
+        {
+            UpdateHeight();
+        }
 
         public void Move(Vector3 direction)
         {
@@ -30,12 +43,19 @@ namespace Basics.Camera
             CheckHeight();
         }
 
+        private void UpdateHeight()
+        {
+            targetHeight.x = transform.position.x;
+            targetHeight.z = transform.position.z;
+            transform.position = Vector3.SmoothDamp(transform.position, targetHeight, ref velocity, heightSmoother);
+        }
+
         private void CheckHeight()
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+            if (Physics.Raycast(targetHeight, Vector3.down, out hit))
             {
-                transform.position -= Vector3.up * (hit.distance - heightFromFloor);
+                targetHeight -= Vector3.up * (hit.distance - heightFromFloor);
             }
         }
     }
