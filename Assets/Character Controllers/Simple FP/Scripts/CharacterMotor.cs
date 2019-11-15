d40b69204e8624328ae6fbdd4e2253c82f50ba40
Archie Andrews <archieandrews@live.co.uk>
2019-11-15 09:29:37 +0000

    added simple charatcer controller

---
+++ Assets/Character Controllers/Simple FP/Scripts/CharacterMotor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMotor : MonoBehaviour
+{
+    private CharacterController cc;
+
+    public Transform headTransform;
+    public float moveSpeed = 5;
+    public float maxPitch = 85, minPitch = -85;
+    public float jumpSpeed = 50;
+
+    private Vector3 yaw;
+    private Vector3 pitch;
+    private float yVelocity = 0;
+
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+        pitch = headTransform.eulerAngles;
+        yaw = transform.eulerAngles;
+    }
+
+    public void MoveCharacter(Vector3 dir, bool jump)
+    {
+        dir = transform.TransformDirection(dir);
+        dir *= moveSpeed;
+
+        if (!cc.isGrounded)
+            yVelocity = Mathf.Clamp(yVelocity + Physics.gravity.y * Time.deltaTime, Physics.gravity.y, Mathf.Infinity);
+
+        if (jump && cc.isGrounded)
+            yVelocity = jumpSpeed;
+
+        if ((cc.collisionFlags & CollisionFlags.Above) != 0)
+            yVelocity = Mathf.Clamp(yVelocity, Physics.gravity.y, 0);
+
+        dir.y = yVelocity;
+
+        cc.Move(dir *= Time.deltaTime);
+    }
+
+    public void RotateCharacter(Vector3 dir)
+    {
+        yaw.x = transform.eulerAngles.x;
+        yaw.y = transform.eulerAngles.y + dir.y;
+        yaw.z = transform.eulerAngles.z;
+
+        transform.rotation = Quaternion.Euler(yaw);
+
+        pitch.x = Mathf.Clamp(pitch.x + dir.x, minPitch, maxPitch);
+        pitch.y = headTransform.eulerAngles.y;
+        pitch.z = headTransform.eulerAngles.z;
+
+        headTransform.rotation = Quaternion.Euler(pitch);
+    }
+}
---
+++ Assets/Character Controllers/Simple FP/Scripts/KC_CharacterInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterMotor))]
+public class KC_CharacterInput : MonoBehaviour
+{
+    private CharacterMotor motor;
+
+    public KeyCode jumpKey = KeyCode.Space;
+
+    private Vector3 moveDirection;
+    private Vector3 rotationDirection;
+    private bool jump = false;
+
+    private void Awake()
+    {
+        motor = GetComponent<CharacterMotor>();
+    }
+
+    private void Update()
+    {
+        moveDirection.x = Input.GetAxis("Horizontal");
+        moveDirection.z = Input.GetAxis("Vertical");
+
+        jump = Input.GetKey(jumpKey);
+
+        motor.MoveCharacter(moveDirection, jump);
+
+        rotationDirection.x = -Input.GetAxis("Mouse Y");
+        rotationDirection.y = Input.GetAxis("Mouse X");
+
+        motor.RotateCharacter(rotationDirection);
+    }
+}
